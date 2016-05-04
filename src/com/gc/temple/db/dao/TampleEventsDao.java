91e1542ebd6228fe2f8435fa8dbd21f5850d2714
Maurice Tedder <maurice_tedder@hotmail.com>
2016-05-04 17:16:39 -0400

    debug hibernate config variable issue

--- src/com/gc/temple/db/dao/TampleEventsDao.java
+++ src/com/gc/temple/db/dao/TampleEventsDao.java
@@ -44,12 +44,12 @@ public class TampleEventsDao {
 			System.out.println(System.getenv("DATABASE_PASSWORD"));
 
 			Configuration cfg = new Configuration();
-			cfg.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
+			//cfg.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
 			cfg.setProperty("hibernate.connection.url", System.getenv("DATABASE_URL_PARAMS"));
 			cfg.setProperty("hibernate.connection.username", System.getenv("DATABASE_USERNAME"));
 			cfg.setProperty("hibernate.connection.password", System.getenv("DATABASE_PASSWORD"));
 			
-			factory = cfg.configure("hibernate.cfg.xml").buildSessionFactory();
+			factory = cfg.configure().buildSessionFactory();
 //			factory = cfg.buildSessionFactory();
 		} catch (Throwable ex) {
 			System.err.println("Failed to create sessionFactory object." + ex);
