b88a5b0c9b00bc52eb0e74feef284ccca960824d
Maurice Tedder <maurice_tedder@hotmail.com>
2016-05-04 16:21:45 -0400

    debug hibernate config variable issue

--- src/com/gc/temple/db/dao/TampleEventsDao.java
+++ src/com/gc/temple/db/dao/TampleEventsDao.java
@@ -41,12 +41,12 @@ public class TampleEventsDao {
 			/**Load the hibernate.cfg.xml from the classpath**/
 			System.out.println(System.getenv("DATABASE_URL_PARAMS"));
 			System.out.println(System.getenv("DATABASE_USERNAME"));
-			System.out.println(System.getenv("DATABASE_PWD"));
+			System.out.println(System.getenv("DATABASE_PASSWORD"));
 
 			Configuration cfg = new Configuration();
 			cfg.setProperty("hibernate.connection.url", System.getenv("DATABASE_URL_PARAMS"));
 			cfg.setProperty("hibernate.connection.username", System.getenv("DATABASE_USERNAME"));
-			cfg.setProperty("hibernate.connection.password", System.getenv("DATABASE_PWD"));
+			cfg.setProperty("hibernate.connection.password", System.getenv("DATABASE_PASSWORD"));
 			
 			factory = cfg.configure().buildSessionFactory();
 //			factory = cfg.buildSessionFactory();
